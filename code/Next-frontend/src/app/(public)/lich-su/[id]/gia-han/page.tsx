18c7dacd1284fd6a175fa73b8b78188c4315e719
trungta897 <nguyentatrunghieu2@gmail.com>
2026-07-01 14:40:42 +0700

    Update page extend request for overdue books

--- code/Next-frontend/src/app/(public)/lich-su/[id]/gia-han/page.tsx
+++ code/Next-frontend/src/app/(public)/lich-su/[id]/gia-han/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 import { MaterialIcon } from "@/components/base/material-icon";
 import { RENEW_PAGE } from "@/constants/ui-text/public";
 import { MOCK_RENEW_DATA } from "@/mocks/loans";
@@ -13,7 +12,6 @@ const formatCurrency = (amount: number) => {
 };
 
 export default function RenewBookPage() {
-    const params = useParams();
     const data = MOCK_RENEW_DATA;
     const { book, currentLateFee, initialDeposit, renewalOptions } = data;
 
